bae810ca16398a5a425a1646a8102fd81b598ac7
Fr4ny3r <guevarafranyer09@gmail.com>
2025-12-05 22:27:55 -0400

    minor modify

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 export default function About() {
     return (
-      <div id="about" className="z-1000 relative bg-[var(--accent)]/12 dark:bg-background/80 backdrop-blur-[0.5rem] rounded-lg p-8 py-12 max-w-3xl w-full">
-        <h1 className="text-4xl relative -left-2 font-bold mb-4"><strong className="rounded-md bg-[var(--secondary)] px-3 py-2">_Fraan_</strong> Minecraft Projects</h1>
+      <>
+    <Link href={`/`} className='flex z-2000 h-15 font-extrabold items-center justify-center text-[var(--primary)]/90 text-[1rem] left-0 top-0 m-8 border-2 border-[var(--accent)]/30 p-3 pr-4 rounded-md pl-2 absolute '>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="relative text-[var(--primary)] icon icon-tabler icons-tabler-outline icon-tabler-chevron-compact-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 20l-3 -8l3 -8" /></svg>
+          Atras
+    </Link>
+      <div className="animFade z-1000 relative bg-[var(--accent)]/12 dark:bg-background/80 backdrop-blur-[var(--blur)] rounded-lg p-8 py-12 max-w-128">
+        <h1 className="text-4xl relative -left-2 font-bold mb-4"><strong className="rounded-md bg-[var(--secondary)] px-3 py-2">_Fraan_</strong> Minecraft</h1>
         <p className="mb-7 mt-8 text-justify">
           Bienvenido a mi sitio web personal, donde comparto mi pasión por Minecraft y mis proyectos relacionados con este increíble juego. Aquí encontrarás información sobre mis aventuras en el mundo de Minecraft, así como detalles sobre los proyectos en los que estoy trabajando actualmente. ¡Explora, diviértete y no dudes en contactarme si tienes alguna pregunta o quieres colaborar en algo genial!
         </p>
@@ -14,5 +21,6 @@ export default function About() {
           <li>Creacion de Texture Packs</li>
         </ul>
       </div>
+      </>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
         // Bienvenido a mi sitio web personal, donde comparto mi pasión por Minecraft y mis proyectos relacionados con este increíble juego. Aquí encontrarás información sobre mis aventuras en el mundo de Minecraft, así como detalles sobre los proyectos en los que estoy trabajando actualmente. ¡Explora, diviértete y no dudes en contactarme si tienes alguna pregunta o quieres colaborar en algo genial!
+import Link from 'next/link';
+import Image from 'next/image';
+
+
 
 export default function page() {
 
@@ -32,19 +36,20 @@ export default function page() {
 
   return (
     <>
-      <div className="z-1000 relative bg-[var(--accent)]/12 dark:bg-background/80 backdrop-blur-[0.5rem] rounded-lg p-8 py-12 max-w-3xl w-full">
-        <h1 className="text-4xl relative -left-2 font-bold mb-4"><strong className="rounded-md bg-[var(--secondary)] px-3 py-2">_Fraan_</strong> Minecraft Projects</h1>
-        <p className="mb-7 mt-8 text-justify">
-          Bienvenido a mi sitio web personal, donde comparto mi pasión por Minecraft y mis proyectos relacionados con este increíble juego. Aquí encontrarás información sobre mis aventuras en el mundo de Minecraft, así como detalles sobre los proyectos en los que estoy trabajando actualmente. ¡Explora, diviértete y no dudes en contactarme si tienes alguna pregunta o quieres colaborar en algo genial!
-        </p>
-        <h2 className="text-2xl font-semibold mb-4">Habilidades</h2>
-        <ul className="list-disc list-inside mb-6">
-          <li>Experiencia en Minecraft en Java Edition</li>
-          <li>Desarrollo de Datapacks</li>
-          <li>Creación y diseño de mapas</li>
-          <li>Desarrollo de Mods</li>
-          <li>Creacion de Texture Packs</li>
-        </ul>
+    <Link href={`/about`} className='flex z-2000 items-center justify-center h-15 font-extrabold rounded-md text-[var(--primary)]/90 text-[1rem] left-0 top-0 m-8 border-2 border-[var(--accent)]/30 p-3 px-4 absolute '>
+      Sobre mi
+    </Link>
+      <div className=" z-1000 relative bg-[var(--accent)]/12 dark:bg-background/80 backdrop-blur-[var(--blur)] rounded-lg p-8 py-12 max-w-128">
+        <div className=" flex justify-center items-center">
+        <Image
+          width={350}
+          height={350}
+          src={"/file.svg"}
+          alt={"random"}
+        />
+        </div>
+        <div className="w-128 relative -left-8 h-2 my-12 bg-[var(--accent)]"></div>
+        <h1 className="text-4xl w-full relative -left-2 font-bold mb-4"><strong className="rounded-md bg-[var(--secondary)] px-3 py-2">_Fraan_</strong> Minecraft</h1>
       </div>
 
     </>
