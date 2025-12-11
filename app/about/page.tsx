00512ea3ef8796853a0beeddb56dc1092710ea1a
Fr4ny3r <guevarafranyer09@gmail.com>
2025-12-11 13:32:42 -0400

    responsive page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function About() {
     return (
       <>
-      <section className="animFade z-1000 relative bg-[var(--accent)]/12 dark:bg-background/80 backdrop-blur-[var(--blur)] rounded-lg p-8 py-12 max-w-128">
+      <section className="animFade contenedor-scroll-personalizado xl:overflow-x-hidden xl:overflow-smooth xl:overflow-y-auto w-full xl:max-w-128 xl:max-h-145 z-1000 relative bg-[var(--accent)]/12 dark:bg-background/80 backdrop-blur-[var(--blur)] rounded-lg p-8 py-12 max-w-128">
         <h1 className="text-2xl md:text-4xl relative -left-2 font-bold mb-4"><strong className="rounded-md bg-[var(--secondary)] px-3 py-2">_Fraan_</strong> Minecraft</h1>
         <p className="mb-7 mt-8 text-justify text-sm md:text-base">
           Bienvenido a mi sitio web personal, donde comparto mi pasión por Minecraft y mis proyectos relacionados con este increíble juego. Aquí encontrarás información sobre mis aventuras en el mundo de Minecraft, así como detalles sobre los proyectos en los que estoy trabajando actualmente. ¡Explora, diviértete y no dudes en contactarme si tienes alguna pregunta o quieres colaborar en algo genial!
